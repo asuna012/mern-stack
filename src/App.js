ab48a78d156701af4d52ec03ab84aad5a91adb6b
Cholo <ctorralba012@gmail.com>
2022-10-10 17:12:32 +0800

    add pages, components to the MERN App.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ import {
 } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
 import Users from "./users/pages/Users";
+import MainNavigation from "./shared/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
---
+++ src/shared/UIElements/Map.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Map.css";
+
+const Map = (props) => {
+  return <div></div>;
+};
+
+export default Map;
